874382ca0838509a3debe210c8b05b7453db8fa1
cailemieuxmack <cailemieuxmack@gmail.com>
2026-03-16 10:49:23 -0500

    ???

--- apr_tool/repair/claude_client.py
+++ apr_tool/repair/claude_client.py
@@ -81,8 +81,9 @@ class ClaudeClient:
             }
         )
 
+        print(f"[DEBUG] Status code: {response.status_code}")
+        print(f"[DEBUG] Response text: {response.text[:500]}")
         data = response.json()
-        print(data["choices"][0]["message"]["content"])
         content = data["choices"][0]["message"]["content"]
         repaired_code = parse_repair_response(content)
         #repaired_code = parse_repair_response(data) # data["choices"][0]["message"]["content"] # 
